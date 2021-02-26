88af1ba60e204f574bb67145325dbd9dcb58af00
Ash Vince <amvince@gmail.com>
2021-02-26 09:30:04 -0800

    7688-2021Test

---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class ArcadeDrive extends CommandBase {
+  /** Creates a new ArcadeDrive. */
+  private static Drivetrain m_drive;
+  private static Joystick m_stick;
+  private static double speed = DriveConstants.MAXSPEED;
+  private static double rot = DriveConstants.MAXTURN;
+  
+  
+  public ArcadeDrive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.m_drive);
+    m_drive = RobotContainer.m_drive;
+    // m_stick = RobotContainer.m_oi;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double rdrive = RobotContainer.m_oi.getRawAxis(1);
+    double rrot = RobotContainer.m_oi.getRawAxis(0);
+    m_drive.arcadeDrive(rdrive*speed, rrot*rot);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,84 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SerialPort;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new drivetrain. */
+  private final WPI_TalonSRX l_talon, r_talon;
+  private final WPI_VictorSPX l_victor, r_victor;
+  private final Encoder r_encoder, l_encoder;
+  private final DifferentialDrive m_drive;
+  private double l_rot, r_rot;
+  private double x_ang, y_ang, z_ang;
+  private AHRS ahrs;
+
+  public Drivetrain() {
+      l_talon = new WPI_TalonSRX(DriveConstants.LDRIVE);
+      r_talon = new WPI_TalonSRX(DriveConstants.RDRIVE);
+      l_victor = new WPI_VictorSPX(DriveConstants.LDRIVE);
+      r_victor = new WPI_VictorSPX(DriveConstants.RDRIVE);
+      l_encoder = new Encoder(DriveConstants.ENCL1, DriveConstants.ENCL2);
+      r_encoder = new Encoder(DriveConstants.ENCR1, DriveConstants.ENCR2, true);
+      l_victor.follow(l_talon);
+      r_victor.follow(r_talon);
+      m_drive = new DifferentialDrive(l_talon, r_talon);
+      l_encoder.reset();
+      r_encoder.reset();
+
+      try {
+        ahrs = new AHRS(SerialPort.Port.kUSB);
+      
+      } catch(RuntimeException ex) {
+        DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+      }
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    l_rot = l_encoder.getDistance();
+    r_rot = r_encoder.getDistance();
+    x_ang = ahrs.getRoll();
+    y_ang = ahrs.getPitch();
+    z_ang = ahrs.getYaw();
+
+    System.out.println("Roll: "+x_ang);
+    System.out.println("Pitch: "+y_ang);
+    System.out.println("Yaw: "+z_ang);
+    
+    /* 
+    System.out.println("Left Encoder: "+l_rot);
+    System.out.println("Right Encoder: "+r_rot);
+    */  
+
+  }
+
+  public void arcadeDrive(double speed, double rot) {
+    m_drive.arcadeDrive(speed, rot);
+  }
+
+  public void tankDrive(double left, double right) {
+    m_drive.tankDrive(left, right);
+  }
+
+  public void stop() {
+    m_drive.tankDrive(0,0);
+  }
+
+  public void resetEncoder() {
+    l_encoder.reset();
+    r_encoder.reset();
+  }
+}
