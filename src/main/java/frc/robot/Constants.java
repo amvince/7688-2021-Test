83ba52e0a05ddcb430282dc289ffba11d90d097a
Ash Vince <amvince@gmail.com>
2021-02-26 09:29:04 -0800

    7688-2021 Initialization

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    public class DriveConstants {
+        final static public int LDRIVE = 1;
+        final static public int RDRIVE = 2;
+
+        final static public int ENCL1 = 0;
+        final static public int ENCL2 = 1;
+        final static public int ENCR1 = 2;
+        final static public int ENCR2 = 3;
+
+        final static public double MAXSPEED = 1;
+        final static public double MAXTURN = 1;
+
+        final static public double WHEELBASE = 25.5; // 25.5 inch wheel base
+        final static public double WHEELRADIUS = 6; // six inch wheels
+    }
+
+    public class Input {
+        final static public int JOYX = 0;
+    }
+
+}
